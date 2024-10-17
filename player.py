ecbabfc1d72a9826e72599e0e16c382c5ee93bd6
Gherasim_Antonie_Stefan <anto.gherasim03@gmail.com>
2024-10-17 03:02:08 +0300

    update ui

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 from collections import Counter
 
-
 class Player:
     def __init__(self, name):
         self.name = name
@@ -19,14 +18,17 @@ class Player:
             possible_formations.append("Full House")
         elif 3 in counts_values:
             possible_formations.append("Three of a Kind")
-        elif sorted(rolled_values) in [list(range(1, 6)), list(range(2, 7))]:
+        if sorted(rolled_values) == [1, 2, 3, 4, 5] or sorted(rolled_values) == [2, 3, 4, 5, 6]:
             possible_formations.append("Large Straight")
-        elif set([1, 2, 3, 4]) <= set(rolled_values) or set([2, 3, 4, 5]) <= set(rolled_values) or set([3, 4, 5, 6]) <= set(rolled_values):
+
+        elif set([1, 2, 3, 4]).issubset(rolled_values) or set([2, 3, 4, 5]).issubset(rolled_values) or set([3, 4, 5, 6]).issubset(rolled_values):
             possible_formations.append("Small Straight")
+ 
+        if counts_values.count(2) == 2:
+            possible_formations.append("Two Pairs")
         elif 2 in counts_values:
-            if counts_values.count(2) == 2:
-                possible_formations.append("Two Pairs")
-            else:
-                possible_formations.append("One Pair")
+            possible_formations.append("One Pair")
 
         return possible_formations
+
+   
